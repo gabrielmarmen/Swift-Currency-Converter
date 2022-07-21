f813653f17771230b1a64ab2cf7f78eea6431cca
Gabriel Marmen <gabrielmarmen@Gabriels-iMac.local>
2022-07-21 18:54:14 +0700

    Base project

---
+++ SwiftCurrencyConverter/SwiftCurrencyConverter/SwiftCurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCurrencyConverterApp.swift
+//  SwiftCurrencyConverter
+//
+//  Created by Gabriel Marmen on 2022-07-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
