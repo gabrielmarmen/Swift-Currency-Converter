53ba5c90960848cdaba42711340b2373543c46b2
Gabriel Marmen <gabton@outlook.com>
2022-10-05 20:24:34 +0700

    Created project

---
+++ Currency Converter/Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Gabriel Marmen on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
